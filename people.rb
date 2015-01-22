508aca69029e67431fac7467d3a0de8f6362a51e
JJLim <lim.jess9@gmail.com>
2015-01-22 13:21:11 -0500

    Created class Person with attribute name and defined an initializer name

--- people.rb
+++ people.rb
@@ -1,10 +1,19 @@
-class Student
+class Person
+  attr_reader :name
+
+  def initialize(name)
+    @name = name    
+  end
+end
+
+
+class Student < Person
   def learn
     "I get it!"
   end
 end
 
-class Instructor
+class Instructor < Person
   def teach
     "Everything in Ruby is an Object"
   end
